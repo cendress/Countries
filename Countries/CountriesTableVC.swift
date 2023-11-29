05f351b868b267ee646a38a0249ad6e36475c136
cendress <95492688+cendress@users.noreply.github.com>
2023-11-29 12:22:52 -0500

    Configure storyboard and parse JSON

--- Countries/CountriesTableVC.swift
+++ Countries/CountriesTableVC.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class CountriesTableVC: UITableViewController {
   
+  var countries: [Country] = []
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     
   }
   
+  func loadJson() {
+    if let url = Bundle.main.url(forResource: "countries", withExtension: "json") {
+      if let data = try? Data(contentsOf: url) {
+        parse(jsonData: data)
+      }
+    }
+  }
+  
+  func parse(jsonData: Data) {
+    let decoder = JSONDecoder()
+    
+    if let decodedData = try? decoder.decode([Country].self, from: jsonData) {
+      countries = decodedData
+      tableView.reloadData()
+    }
+  }
 }
