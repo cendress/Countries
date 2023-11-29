94cb0e6eabf7ff2df2d4ad3c826ea99ba00dec82
cendress <95492688+cendress@users.noreply.github.com>
2023-11-29 12:02:15 -0500

    Create country struct

---
+++ Countries/Country.swift
@@ -0,0 +1,16 @@
+//
+//  Country.swift
+//  Countries
+//
+//  Created by Christopher Endress on 11/29/23.
+//
+
+import Foundation
+
+struct Country {
+  let name: String
+  let capital: String
+  let size: String
+  let population: Int
+  let currency: String
+}
