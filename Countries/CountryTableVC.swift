702ab5c71fb2ccc0e52224fac46d470dd894fa7f
cendress <95492688+cendress@users.noreply.github.com>
2023-11-29 12:12:26 -0500

    Add CountriesVC

--- Countries/CountryTableVC.swift
+++ Countries/CountryTableVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CountryTableVC.swift
 //  Countries
 //
 //  Created by Christopher Endress on 11/29/23.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class CountryTableVC: UITableViewController {
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
   }
-
-
+  
 }
-
